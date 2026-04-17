c52e6ad9b050893b7cdfad1f7718a074e89345a4
pendar <a_pendar@hotmail.com>
2026-04-17 18:10:36 +0200

    add ORDER BY query

---
+++ basics/order_by.sql
@@ -0,0 +1,7 @@
+-- Sort employees by salary (highest to lowest)
+SELECT * FROM employees
+ORDER BY salary DESC;
+
+-- Sort names by salary (lowest to highest)
+SELECT name FROM employees
+ORDER BY salary ASC;
